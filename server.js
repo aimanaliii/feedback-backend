ac88acbd31689f4bc902b4331fa0501f4f4c5f6f
aimanaliii <aimanali3568@gmail.com>
2025-03-23 04:10:27 +0500

    Fix duplicate path declaration

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
-const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
